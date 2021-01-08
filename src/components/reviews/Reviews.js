e18f159b6de8fe53bd6d241a42cd6a5c5d29683d
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2021-01-07 19:28:59 -0600

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,11 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const { reviews, deleteReview, restaurantId } = this.props;
+    const restaurantReviews = reviews.filter(review => review.restaurantId === restaurantId);
     return (
       <ul>
-        Reviews
+        {restaurantReviews.map((review) => <Review key={review.id} review={review} deleteReview={deleteReview}/>)}
       </ul>
     );
   }
